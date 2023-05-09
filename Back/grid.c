941de65ae11ecf94cc6a98021f1ce387bd5e27f1
Guilherme Coelho Vieira <92694963+YtsDarko04@users.noreply.github.com>
2023-05-09 16:28:53 +0200

    Update grid.c

--- Back/grid.c
+++ Back/grid.c
@@ -1,1 +1,4 @@
+//Partie Module : 
+//Importons depuis le fichier Front le fichier sudoku.c
 
+//Création des méthodes qui nous seront utiles pour créer la grille de jeu, ainsi que des méthodes qui seront appelées pour gérer le jeu
