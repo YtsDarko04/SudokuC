81e01b5ac046dc3f6a0e5300905a4ab40b0d22a9
Guilherme Coelho Vieira <92694963+YtsDarko04@users.noreply.github.com>
2023-05-09 16:30:09 +0200

    Update playerchoice.c

--- Back/playerchoice.c
+++ Back/playerchoice.c
@@ -1,1 +1,5 @@
+//Partie Module : 
+//Importons depuis le fichier Back le fichier grid.c
+//Importons depuis le fichier Front le fichier sudoku.c
 
+//Création des méthodes qui nous seront utiles pour gérer l'interaction du joueur avec la grille de jeu, ainsi que des méthodes qui seront appelées pour gérer le jeu.
