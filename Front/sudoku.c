a0866e05a4096b665c2eb5fe5e6dfc09525c074d
Guilherme Coelho Vieira <92694963+YtsDarko04@users.noreply.github.com>
2023-05-09 16:27:02 +0200

    Update sudoku.c

--- Front/sudoku.c
+++ Front/sudoku.c
@@ -1,1 +1,6 @@
+//Partie Module : 
+//Importons depuis le fichier Back le fichier grid.c
+//Importons depuis le fichier Back le fichier playerchoice.c
 
+//Partie Vue :
+//Création de la fonction jeu, qui appelera les méthodes présentes dans nos fichiers importées
